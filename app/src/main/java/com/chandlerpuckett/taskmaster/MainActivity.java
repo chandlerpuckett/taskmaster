37cdfc98c9b8de2cb60a70e8861fc2380c6aa2eb
Chandler Puckett <chandler.puckett1@gmail.com>
2020-10-19 19:58:15 -0700

    buttons on main page working

--- app/src/main/java/com/chandlerpuckett/taskmaster/MainActivity.java
+++ app/src/main/java/com/chandlerpuckett/taskmaster/MainActivity.java
@@ -2,17 +2,49 @@ package com.chandlerpuckett.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button addTaskButton;
+    private Button allTasksButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        setContentView(R.layout.activity_add_task);
-        setContentView(R.layout.activity_all_tasks);
+        setContentView(R.layout.activity_main);
+
+        addTaskButton = (Button) findViewById(R.id.addTask);
+        addTaskButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openAddTaskPage();
+            }
+        });
+
+        allTasksButton = (Button) findViewById(R.id.allTasks);
+        allTasksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAllTasksPage();
+            }
+        });
+
     }
 
+    public void openAddTaskPage(){
+        Intent intent = new Intent(this, AddTask.class);
+        startActivity(intent);
+    }
+
+    public void openAllTasksPage(){
+        Intent intent = new Intent(this, AllTasks.class);
+        startActivity(intent);
+    }
 
 }

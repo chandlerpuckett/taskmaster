f09f9fe2d7461a205632a683cabb488d58c1272c
Chandler Puckett <chandler.puckett1@gmail.com>
2020-11-02 19:08:22 -0800

    app crashing upon sign up, Auth plug-in not found

--- app/src/main/java/com/chandlerpuckett/taskmaster/SignUpActivity.java
+++ app/src/main/java/com/chandlerpuckett/taskmaster/SignUpActivity.java
@@ -1,20 +1,16 @@
 package com.chandlerpuckett.taskmaster;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.amplifyframework.AmplifyException;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.amplifyframework.auth.AuthUserAttributeKey;
-import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.auth.options.AuthSignUpOptions;
 import com.amplifyframework.core.Amplify;
-
-import org.w3c.dom.Text;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -22,14 +18,6 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
-        // Add this line, to include the Auth plugin.
-        try {
-            Amplify.addPlugin(new AWSCognitoAuthPlugin());
-            Amplify.configure(getApplicationContext());
-        } catch (AmplifyException e) {
-            e.printStackTrace();
-        }
 
         ((Button) findViewById(R.id.userSignUpBtn)).setOnClickListener(view ->{
             String username = ((TextView) findViewById(R.id.signUpUsername)).getText().toString();
